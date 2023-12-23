4351168e64b3a18055146eb27b02d62fedb9a64e
samet <205541038@firat.edu.tr>
2023-12-23 08:59:32 +0300

    logincomp

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  form: FormGroup = this.fb.group({
+    emailAdress: ['', Validators.required],
+    passwword: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) {}
 }
